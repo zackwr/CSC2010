4f1f994ca11ff57c9b056ef34f103497076e8101
zackwr <zackscary@gmail.com>
2015-09-16 12:22:59 -0400

    Second time around

--- MuchBetter.java
+++ MuchBetter.java
@@ -1,7 +1,7 @@
 public class MuchBetter {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println("A \"quoted\" String is");
-		System.out.println("'much better if you can learn");
+		System.out.println("'much' better if you learn");
 		System.out.println("the rules of \"escape sequences.\"");
 		System.out.println();
 		System.out.println("Also, \"\" represents an empty String.");
